148f81f70c98df32076ca9e597e47d0419aae968
Stephanie Leung <stephanie@murphyshome.net>
2025-11-06 12:22:36 +0800

    remove unused dependency from deleted file

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import {SidebarProvider} from "@/components/ui/sidebar";
 import AppSidebar from "@/components/app-sidebar";
 
 export default function MainLayout({
@@ -9,12 +8,9 @@ export default function MainLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <SidebarProvider>
-            <div className="flex min-h-screen w-full bg-[var(--dark)]">
-                <AppSidebar />
-                <main className="grow p-6">{children}</main>
-            </div>
-        </SidebarProvider>
-
+        <div className="flex min-h-screen w-full bg-[var(--dark)]">
+            <AppSidebar />
+            <main className="grow p-6">{children}</main>
+        </div>
     );
 }
